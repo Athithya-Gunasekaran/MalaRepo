ba9e256739d7dd4680b703e774aa9a5742dd7317
Athithya <athithyaguna@gmail.com>
2022-06-02 13:31:51 -0400

    new two lines added

--- src/Test3.java
+++ src/Test3.java
@@ -4,6 +4,11 @@ public class Test3 {
 	public static void main(String[] args) {
 		
 		System.out.println(" print java2");
+		
+		System.out.println(" print branch1");
+		
+		System.out.println(" print branch2");
+		
 
 	}
 
